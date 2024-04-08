c6d065af3823437c7373d896eea8bfda2bdb97ce
CharleryM <charlerymalcolm.l@google.com>
2024-04-08 08:52:31 +0200

    titre

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Falium
 Un langage de prise de note et d'écriture de formules facile et rapide. Ce language dédié aux étudiant et professeurs aussi bien litéraire que sciantifique. Ce langage prendra en charge différantes polices d'écriture et un maximum de sytaxe mathématique. Il sera inspiré du Markdown et d'autre tel que le LaTeX.
+
+Les commendes :
+
+- entourer le texte de * pour mettre en *italique*
+- entourer le texte de ** pour mettre en **gras**
+- entourer le texte de _ pour mettre en souligné
+- entourer le texte de ` pour mettre en code
+- entourer le texte de ~~ pour mettre en railler
+- commencer la ligne par # pour le titre plus vous en ajouter a la suite plus le tritre sera petit avec un limite de taille de 6
+- § permet de faire un ligne de séparation.
+
+        `ont test`
--- scripts/polices.ts
+++ scripts/polices.ts
@@ -1,44 +1,50 @@
 export class Polices {
 
-    static Strong(input: string): string {
+    static Strong(input: string): string {             //gras
         return input.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
     }
 
-    static Italic(input: string): string {
+    static Italic(input: string): string {             //italique
         return input.replace(/\*(.*?)\*/g, '<em>$1</em>');
     }
 
-    static Mocked(input: string): string {
+    static Mocked(input: string): string {             //barré
         return input.replace(/\~\~(.*?)\~\~/g, '<s>$1</s>');
     }
 
-    static Underline(input: string): string {
+    static Underline(input: string): string {          //souligné
         return input.replace(/\_(.*?)\_/g, '<u>$1</u>');
     }
 
-    static Code(input: string): string {
+    static Code(input: string): string {               //bloque de code
         return input.replace(/\`(.*?)\`/g, '<code>$1</code>');
     }
-    static Title(input: string): string[]{
+    static Title(input: string): string {              //gestion des titre limiter à unetalle de h6
         let taille_title: number = 0;
         let lines: string[] = input.split('\n')
         let hashtag: string[] = []
-        let title: string[] =[]
+        let title: string[] = []
         lines.forEach((line) => {
             if (line.startsWith("#")) {
                 hashtag = line.split(' ');
                 taille_title = (hashtag[0].length);
-                title.push(`<h${taille_title}>${hashtag[1]}</h${taille_title}>\n`)
+                if (taille_title > 6) {
+                    title.push(`${line}`)
+                } else {
+                    title.push(`<h${taille_title}>${hashtag[1]}</h${taille_title}>\n`)
+                }
             } else {
                 title.push(`${line}\n`)
             }
         })
-        return title
+        let file: string = title.join('')
+        return file
     };
 }
 
+
 export class Gestion {
-    static horizontalBreck(input: string): string {
+    static horizontalBreck(input: string): string {    //barre horisontal de séparation
         return input.replace(/§/g, '<hr>')
     }
 }
